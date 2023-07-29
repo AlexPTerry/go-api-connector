5497792a788838298e16ab133fd55c35336cdb75
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2023-07-29 20:24:55 +0100

    Initialised project

---
+++ src/main/java/alex/goapiconnector/apiconnector/GoApiConnectorApplication.java
@@ -0,0 +1,13 @@
+package alex.goapiconnector.apiconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoApiConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoApiConnectorApplication.class, args);
+	}
+
+}
